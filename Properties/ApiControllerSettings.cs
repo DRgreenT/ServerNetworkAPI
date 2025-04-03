daae26d86db381b723923e009cd2e67c135c283e
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-03 12:34:51 +0300

    renamed

---
+++ Properties/ApiControllerSettings.cs
@@ -0,0 +1,6 @@
+public class ApiControllerSettings
+{
+    public static readonly string WebApiPort = "5050";
+    public const string WebApiName = "network";
+    public static readonly int timeOut = 15;
+}
