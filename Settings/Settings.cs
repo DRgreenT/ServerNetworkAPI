e301685fb1a74487968ca8570c269de72bd2f8e4
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-03 20:36:39 +0300

    moved

--- Settings/Settings.cs
+++
@@ -1,7 +0,0 @@
-public class Settings
-{
-    // Initalize default values
-    public static string WebApiPort = "5050";
-    public static string WebApiName = "network";
-    public static int timeOut = 15;
-}
