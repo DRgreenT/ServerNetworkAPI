3bf8aba1f772ab7cd7286b0a488603ecf96ac718
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-09 21:08:31 +0300

    AppConfigData class created

---
+++ dev/Models/AppConfigData.cs
@@ -0,0 +1,12 @@
+﻿namespace ServerNetworkAPI.dev.Models
+{
+    public class AppConfigData
+    {
+        public string FallbackIpMask { get; set; } = "192.168.178.";
+        public int ScanIntervalSeconds { get; set; } = 15;
+        public bool IsNmapEnabled { get; set; } = false;
+        public int WebApiPort { get; set; } = 5050;
+        public string WebApiControllerName { get; set; } = "network";
+        public int MaxIPv4AddressWithoutWarning { get; set; } = 190;
+    }
+}
