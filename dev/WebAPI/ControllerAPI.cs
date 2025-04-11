b812e2a1cde8518a848785e4d4eef7318075f6e6
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-11 08:25:34 +0300

    renamed and log controller added

---
+++ dev/WebAPI/ControllerAPI.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerNetworkAPI.dev.Core;
+using ServerNetworkAPI.dev.Models;
+
+namespace ServerNetworkAPI.dev.WebAPI
+{
+    [ApiController]
+    [Route("api")]
+    public class ControllerAPI : ControllerBase
+    {
+        public static string NetworkControllerName { get; private set; } = @"api/network";
+        
+        [HttpGet("network")]
+        public IActionResult GetDevices()
+        {
+            var devices = NetworkContext.GetDevices();
+            return Ok(devices);
+        }
+
+        public static string LogControllerName { get; private set; } = @"api/log";
+
+        [HttpGet("log")]
+        public IActionResult GetLogs()
+        {
+            var logData =  LogData.GetLogData();
+            return Ok(logData);
+        }
+    }
+}
