d99b9f6b14d4227a20a22388cf121109b082e087
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-15 17:29:01 +0300

    update for new webUI

--- build_linux_publish.bat
+++ build_linux_publish.bat
@@ -6,16 +6,15 @@ setlocal
 set PROJECT_NAME=ServerNetworkAPI
 set CONFIG=Release
 set RUNTIME=linux-x64
-set FRONTEND_SOURCE=WebFrontEnd
+set FRONTEND_SOURCE=dev\WebUI
 
 :: === OUTPUT PATHS ===
 set OUTPUT=publish
 set OUTPUT_API=%OUTPUT%\linux
-set OUTPUT_FE=%OUTPUT%\WebFrontEnd
+set OUTPUT_FE=%OUTPUT_API%\wwwRoot
 set OUTPUT_ZIP_FOLDER=publish\zip
 
 set ZIPFILE_API=%OUTPUT_ZIP_FOLDER%\ServerNetworkAPI.zip
-set ZIPFILE_FE=%OUTPUT_ZIP_FOLDER%\frontEndTemplate.zip
 
 echo ========================
 echo CLEANING PREVIOUS BUILD
@@ -48,6 +47,14 @@ if exist %OUTPUT_API%\publish (
     rd /s /q %OUTPUT_API%\publish
 )
 
+echo ========================
+echo COPYING FRONTEND TO PUBLISH FOLDER
+echo ========================
+if exist %OUTPUT_FE% (
+    rd /s /q %OUTPUT_FE%
+)
+xcopy /e /i /y %FRONTEND_SOURCE% %OUTPUT_FE%
+
 :: === Ensure ZIP folder exists ===
 if not exist %OUTPUT_ZIP_FOLDER% (
     echo Creating ZIP folder: %OUTPUT_ZIP_FOLDER%
@@ -68,27 +75,6 @@ if exist %ZIPFILE_API% (
     echo Failed to create ZIP (API)
 )
 
-echo ========================
-echo CREATING ZIP ARCHIVE (Frontend)
-echo ========================
-if exist %ZIPFILE_FE% (
-    echo Previous ZIP exists. Deleting...
-    del /f /q %ZIPFILE_FE%
-)
-powershell -Command "Compress-Archive -Path '%FRONTEND_SOURCE%\*' -DestinationPath '%ZIPFILE_FE%' -Force"
-if exist %ZIPFILE_FE% (
-    echo ZIP created: %ZIPFILE_FE%
-) else (
-    echo Failed to create ZIP (Frontend)
-)
-
-echo ========================
-echo COPYING FRONTEND TO PUBLISH FOLDER
-echo ========================
-if exist %OUTPUT_FE% (
-    rd /s /q %OUTPUT_FE%
-)
-xcopy /e /i /y %FRONTEND_SOURCE% %OUTPUT_FE%
 echo ========================
 echo ALL DONE
 echo ========================
