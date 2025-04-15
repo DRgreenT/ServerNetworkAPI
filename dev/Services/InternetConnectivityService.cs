f5d1c0717ea0fe74fc30fa28e6facc964616098e
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-15 17:39:30 +0300

    basic check for internet connection

---
+++ dev/Services/InternetConnectivityService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ServerNetworkAPI.dev.Services
+{
+    public class InternetConnectivityService
+    {
+        private static readonly string[] _testUrls =
+        {
+            "https://www.google.com",
+            "https://www.cloudflare.com"
+        };
+
+        private static readonly TimeSpan _timeout = TimeSpan.FromSeconds(5);
+
+        public static async Task<bool> IsInternetAvailableAsync()
+        {
+            using var client = new HttpClient { Timeout = _timeout };
+
+            foreach (var url in _testUrls)
+            {
+                try
+                {
+                    var response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                }
+                catch
+                {
+                    // Ignore and try next
+                }
+            }
+
+            return false;
+        }
+    }
+}
