d9ee93e67d5554ea274793d005928df24d505e3d
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-15 17:43:13 +0300

    new API controllers added
    api/data, api/notes

--- dev/WebAPI/ControllerAPI.cs
+++ dev/WebAPI/ControllerAPI.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServerNetworkAPI.dev.Core;
 using ServerNetworkAPI.dev.Models;
+using ServerNetworkAPI.dev.Models.Enums;
+using ServerNetworkAPI.dev.Services;
 
 namespace ServerNetworkAPI.dev.WebAPI
 {
@@ -8,7 +10,7 @@ namespace ServerNetworkAPI.dev.WebAPI
     [Route("api")]
     public class ControllerAPI : ControllerBase
     {
-        public static string NetworkControllerName { get; private set; } = @"api/network";
+        public static string NetworkControllerName { get;} = @"api/network";
         
         [HttpGet("network")]
         public IActionResult GetDevices()
@@ -17,13 +19,41 @@ namespace ServerNetworkAPI.dev.WebAPI
             return Ok(devices);
         }
 
-        public static string LogControllerName { get; private set; } = @"api/log";
+        public static string LogControllerName { get;} = @"api/log";
 
         [HttpGet("log")]
         public IActionResult GetLogs()
         {
             var logData =  LogData.GetLogData();
             return Ok(logData);
+        }
+
+        public static string NotificationControllerName { get; } = @"api/notes";
+
+        [HttpGet("notes")]
+        public IActionResult GetNotes()
+        {
+
+            var noteData = NotificationData.GetNotificationData();
+            return Ok(noteData);
+        }
+
+        public static string DataControllerName { get; } = @"api/data";
+
+        [HttpGet("data")]
+        public IActionResult GetApiData()
+        {
+            try
+            {
+                var noteData = TasksBackgroundService.apiData;
+                return Ok(noteData);
+            }
+            catch(Exception ex)
+            {
+                LogData.NewData("DataApiController","[Exeption -> ApiData]", MessageType.Exception,ex.Message);
+                return BadRequest(ex.Message);
+            }
+
         }
     }
 }
