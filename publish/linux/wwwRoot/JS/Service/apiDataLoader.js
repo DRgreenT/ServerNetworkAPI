02444718149818dd884010f38972996d878d3bfb
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-04-30 22:53:44 +0300

    api´s splitted

--- publish/linux/wwwRoot/JS/Service/apiDataLoader.js
+++ publish/linux/wwwRoot/JS/Service/apiDataLoader.js
@@ -17,20 +17,25 @@ async function getValues() {
 async function fetchApiData() {
     while (isGetData) {
         try {
-            const response = await fetch('/api/data');
+            const responseUiData = await fetch('/api/data');
+            const responseNetworkData = await fetch('/api/network');
 
-            if (!response.ok) {
-                throw new Error(`HTTP error ${response.status}`);
+            if (!responseUiData.ok) {
+                throw new Error(`UiData: HTTP error ${responseUiData.status}`);
+            }
+            if (!responseNetworkData.ok) {
+                throw new Error(`NetworkData: HTTP error ${responseNetworkData.status}`);
             }
 
-            let allApiData = await response.json();
+            let allUiData = await responseUiData.json();
+            let allNetworkData = await responseNetworkData.json();
 
-            version = allApiData.version;
-            noteData = allApiData.notifications;
-            devicesData = allApiData.devices;
-            logData = allApiData.log;
-            arrActiveDeviceCounts = allApiData.activeDevicesCounts;
-            systemData = allApiData.systemInfo;
+            version = allUiData.version;
+            noteData = allUiData.notifications;
+            devicesData = allNetworkData;
+            logData = allUiData.log;
+            arrActiveDeviceCounts = allUiData.activeDevicesCounts;
+            systemData = allUiData.systemInfo;
 
             if (!uiData.activeDeviceCount) uiData.activeDeviceCount = 0;
             if (!uiData.totalDeviceCount) uiData.totalDeviceCount = 0;
@@ -39,12 +44,12 @@ async function fetchApiData() {
             if (uiData.isInternetAvailable === undefined) uiData.isInternetAvailable = false;
             if (uiData.isNmapEnabled === undefined) uiData.isNmapEnabled = false;
 
-            uiData.activeDeviceCount = allApiData.activeDeviceCount;
-            uiData.totalDeviceCount = allApiData.totalDeviceCount;
-            uiData.lastUpdateTime = allApiData.lastUpdateTime;
-            uiData.uptime = allApiData.uptime;
-            uiData.isInternetAvailable = allApiData.isInternetAvailable;
-            uiData.isNmapEnabled = allApiData.isNmapEnabled;
+            uiData.activeDeviceCount = allUiData.activeDeviceCount;
+            uiData.totalDeviceCount = allUiData.totalDeviceCount;
+            uiData.lastUpdateTime = allUiData.lastUpdateTime;
+            uiData.uptime = allUiData.uptime;
+            uiData.isInternetAvailable = allUiData.isInternetAvailable;
+            uiData.isNmapEnabled = allUiData.isNmapEnabled;
 
         } catch (err) {
             console.error('Error fetching or processing data:', err);
