88ee30d4637987101ceba7ea7c82288b16dffb34
Thomas Just <124365904+DRgreenT@users.noreply.github.com>
2025-05-01 02:21:31 +0300

    new dataType

---
+++ dev/Models/NmapData.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerNetworkAPI.dev.Models
+{
+    public class NmapData
+    {
+        public string OS { get; set; } = string.Empty;
+        public string MACAddress { get; set; } = string.Empty;
+        public string NetworkDistance { get; set; } = string.Empty;
+        public List<OpenPorts> OpenPorts { get; set; } = new();
+
+    }
+}
